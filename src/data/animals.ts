b41b88f78f043a7d8b05429c76489c84b4f9a98d
M Haidar Hanif <git@mhaidarhanif.com>
2024-10-06 17:40:20 +0700

    feat: get all animals & by id

---
+++ src/data/animals.ts
@@ -0,0 +1,5 @@
+export const dataAnimals = [
+  { id: "a", name: "Cat", color: "orange" },
+  { id: "b", name: "Dog", color: "black" },
+  { id: "c", name: "Rabbit", color: "white" },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { logger } from "hono/logger";
 
-const app = new Hono()
+import { dataAnimals } from "./data/animals";
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-export default app
+app.use(logger());
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Animalia API",
+  });
+});
+
+app.get("/animals", (c) => {
+  return c.json(dataAnimals);
+});
+
+app.get("/animals/:id", (c) => {
+  const id = c.req.param("id");
+
+  const animal = dataAnimals.find((animal) => animal.id === id);
+
+  return c.json(animal);
+});
+
+export default app;
